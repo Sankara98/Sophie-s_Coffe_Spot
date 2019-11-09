4ac0f80d4edb3f8416291e2dadec62b5fee97f44
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2019-11-08 22:23:29 -0500

    BackgroundSection Component Created:     - Added Background component to home page     -Styled BackgroundSection of the home page

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,16 +7,14 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./layout.css";
 
 import Navbar from './Global/Navbar';
 
 const Layout = ({ children }) => <>
-{children}
 <Navbar/>
+{children}
 </>
 
 Layout.propTypes = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import React from "react"
-import { Link } from "gatsby"
+import {graphql} from "gatsby"
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {FaGulp} from "react-icons/fa"
-
-const IndexPage = () => (
+import BackgroundSection from '../components/Global/BackgroundSection';
+const IndexPage = ({data}) => (
   <Layout>
     <SEO title="Home" />
+    <BackgroundSection 
+    img={data.img.childImageSharp.fluid} 
+    title="Sophie's Coffee Spot"
+    styleClass="default-background"
+    />
   </Layout>
-)
+);
 
+
+export const query = graphql`
+{
+  img:file(relativePath:{eq:"default-background.jpeg"}){
+    childImageSharp{
+      fluid{
+        ...GatsbyImageSharpFluid_tracedSVG
+			}
+		}
+	}
+}
+`;
 export default IndexPage
